9376139cdbbc30827f14d51acab35ee24a7480f4
Sgalstyan06 <96201224+Sgalstyan06@users.noreply.github.com>
2023-12-13 12:40:35 +0400

    add db file

--- script.js
+++ script.js
@@ -1,4 +1,77 @@
-console.log("me")
-function sayHello(){
-    console.log("say hello")
+
+'use strict';
+
+let post = document.querySelector('.post');
+let get = document.querySelector('.get');
+let putch = document.querySelector('.putch');
+let put = document.querySelector('.put');
+let del = document.querySelector('.del');
+post.addEventListener('click', postData)
+get.addEventListener('click', getData)
+putch.addEventListener('click', putchData)
+put.addEventListener('click', putData)
+del.addEventListener('click', delData)
+
+function postData (){
+fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      userId: 2,
+      title: "buy vegetables",
+      autor: 'some text'
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"	
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
+
+function getData(){
+fetch('http://localhost:3000/posts')
+.then(res=>{
+  return res.json();
+  
+}).then(res=>{
+  console.log(res);
+})
+}
+//polnaya novaya versia
+function putData(){
+  fetch('http://localhost:3000/posts/2', {
+      method: 'PUT',
+      body: JSON.stringify({
+        userId: 22,
+        id: 22,
+        title: "updated resource",
+        completed: false
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"	
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+}
+
+function putchData(){
+  fetch('http://localhost:3000/posts/2', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: "my custom title"
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"	
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+function delData(){
+  fetch('http://localhost:3000/posts/2', {
+    method: 'DELETE'
+  })
 }
